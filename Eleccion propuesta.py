b78f05abe890cfa2a729e64fe2113d62c4c2197c
Daniel Villalba <79503652+dandrovil@users.noreply.github.com>
2024-06-07 02:51:33 -0300

    Delete Eleccion propuesta.py
    reorganizando

--- Eleccion propuesta.py
+++
@@ -1,7 +0,0 @@
-
-import random
-
-propuestas = ["Desarrollar un sistema de gestión para un estacionamiento de autos.","Desarrollar Un Sistema de Administracion de Franquiciados/Empleados.","Desarrollar Un Sistema Para un Taller Mecanico.","Desarrollar Un Sistema de administracion/organizacion de estudios medicos.","Desarrollar un sistema de gestion para una academia virtual."]
-
-propuesta_aleatoria = random.choice(propuestas)
-print("La propuesta ganadora es: ", propuesta_aleatoria)
