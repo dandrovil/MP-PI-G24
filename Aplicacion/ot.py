ed238341962f75101c5eb8b47dd6d8278726bfd2
Daniel Villalba <79503652+dandrovil@users.noreply.github.com>
2024-06-14 09:14:48 -0300

    Modulo ordenes de trabajo

--- Aplicacion/ot.py
+++ Aplicacion/ot.py
@@ -2,6 +2,8 @@ import vehiculos, mecanicos
 import datetime
 import sql
 
+# la función crear() permite registrar una nueva orden de trabajo para un vehículo específico en una base de datos, 
+# solicitando la patente del vehículo y el motivo de la avería, y luego almacenando esta información junto con la fecha actual en la tabla ordenes
 def crear():
     x=datetime.datetime.now()
     patente = input("Ingrese Patente: ")
@@ -15,18 +17,24 @@ def crear():
     sql.cursor.execute(query, values)
     sql.conexion.commit() 
     print("Orden creada con exito.")
-    
+ 
+#la función consultar(id) busca y muestra todas las órdenes de trabajo que tienen un estado específico (id) en la base de datos, 
+# formateando y mostrando los detalles relevantes de cada orden si se encuentran.    
 def consultar(id):
     query = "SELECT * FROM ordenes WHERE estado = %s"
     values = (id,)
     sql.cursor.execute(query,values)
     ordenes = sql.cursor.fetchall()
     if ordenes:
+        print("Ordenes de trabajo")
+        print("------------------")
         for orden in ordenes:
             print(orden[0], orden[1], orden[2].strftime("%Y-%m-%d"), orden[3], orden[4], orden[5], orden[6])
     else:
         print("No hay ordenes con ese estado en la base de datos.")
 
+#la función asignar() facilita la asignación de una orden de trabajo a un mecánico específico en la base de datos, 
+# verificando la existencia de la orden y del mecánico antes de realizar la asignación.
 def asignar():
     consultar("ingresado")
     nroorden = input("Ingrese N° de orden a asignar: ")
@@ -44,6 +52,8 @@ def asignar():
     else:
         print(f"El N° de Orden {nroorden} NO existe.")
 
+# la función mostrar_ordenes() realiza una consulta a la base de datos para recuperar todas las órdenes almacenadas
+# y las muestra en la consola. Si no hay órdenes disponibles, informa al usuario de esta situación.
 def mostrar_ordenes():
     query = "SELECT * FROM ordenes"
     sql.cursor.execute(query)
@@ -54,11 +64,23 @@ def mostrar_ordenes():
     else:
         print("No hay ordenes en la base de datos.")
     
+
+#la función buscar(id) permite verificar la existencia de una orden específica en la base de datos a través de su idOrden, 
+# devolviendo True si la orden existe y False si no existe.
 def buscar(id):
     query = "SELECT * FROM ordenes WHERE idOrden = %s"
     values = (id,)
     sql.cursor.execute(query, values)
+    sql.cursor.fetchone()
     if(sql.cursor.rowcount == 1):
         return True
     else:
         return False
+
+#la función update(id, status) permite modificar el estado de una orden específica en la base de datos    
+def update(id, status):
+    query = "UPDATE ordenes SET estado = %s WHERE idOrden = %s"
+    values = (status, id)
+    sql.cursor.execute(query, values)
+    sql.conexion.commit()
+    
